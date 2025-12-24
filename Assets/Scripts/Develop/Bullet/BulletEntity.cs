477481e70be30f2f9805b21cad3aa950459f07f8
HIBIKI_5201 <takuto5201@gmail.com>
2025-12-25 01:03:02 +0900

    [update]弾丸から移動関係のパラメータを削除

--- Assets/Scripts/Develop/Bullet/BulletEntity.cs
+++ Assets/Scripts/Develop/Bullet/BulletEntity.cs
@@ -1,10 +1,7 @@
 using Unity.Entities;
-using Unity.Mathematics;
 
 public struct BulletEntity : IComponentData
 {
-    public float3 Direction;
-    public float Speed;
     public float Radius;
 }
 
