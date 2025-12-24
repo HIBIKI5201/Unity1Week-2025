f0bb28f55dcba942f9ae0f7d54850203dc5d7694
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-24 16:40:03 +0900

    [update]playecontroller

---
+++ Assets/Scripts/Develop/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerConfig _config;
+    private InputBuffer _inputBuffer;
+    private PlayerMover _playerMover;
+    private Vector2 _moveDirection;
+
+
+    private void Start()
+    {
+        _inputBuffer = GetComponent<InputBuffer>();
+        InitialRegistration();
+        PlayerMover playerMover = new PlayerMover(_config, transform);
+    }
+
+    private void OnDestroy()
+    {
+        UnRegistrantion();
+    }
+
+    private void Update()
+    {
+        _playerMover.OnMove(_moveDirection, Time.deltaTime);
+    }
+
+    private void InitialRegistration()
+    {
+        _inputBuffer.PlayerMove.performed += OnMove;
+        _inputBuffer.PlayerMove.canceled += OnMove;
+    }
+
+    private void UnRegistrantion()
+    {
+        _inputBuffer.PlayerMove.performed -= OnMove;
+        _inputBuffer.PlayerMove.canceled -= OnMove;
+    }
+
+    private void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _moveDirection=context.ReadValue<Vector2>();
+        }
+        else if(context.canceled)
+        {
+            _moveDirection=Vector2.zero;
+        }
+    }
+}
