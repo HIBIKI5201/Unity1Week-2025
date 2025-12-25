cfcb5fd5723ac3dca53106da106d6bf5cb6e623d
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-25 13:22:48 +0900

    [update]plaeyrattack

--- Assets/Scripts/Develop/Player/InputBuffer.cs
+++ Assets/Scripts/Develop/Player/InputBuffer.cs
@@ -5,15 +5,19 @@ using UnityEngine.InputSystem;
 public class InputBuffer : MonoBehaviour
 {
     private const string PLAYER_MOVE = "Move";
+    private const string PLAYER_ATTACK = "Attack";
 
     public InputAction PlayerMove => _playerMove;
+    public InputAction PlayerAttack => _playerAttack;
     private InputAction _playerMove;
+    private InputAction _playerAttack;
 
     private void Awake()
     {
         if(TryGetComponent<PlayerInput>(out var playerInput))
         {
             _playerMove = playerInput.actions[PLAYER_MOVE];
+            _playerAttack = playerInput.actions[PLAYER_ATTACK];
         }
     }
 }
