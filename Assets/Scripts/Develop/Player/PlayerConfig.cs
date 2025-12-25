b52062dd8a362c399674aafcef71f831e09e699a
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-25 13:23:05 +0900

    [add]confgi

--- Assets/Scripts/Develop/Player/PlayerConfig.cs
+++ Assets/Scripts/Develop/Player/PlayerConfig.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerConfig : ScriptableObject
 {
     public float CollisionRadius => _collisionRadius;
+    public float AttackSpeed => _attackSpeed;
     public float MoveSpeed => _moveSpeed;
     [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _attackSpeed = 1f;
     [SerializeField] private float _collisionRadius = 1.5f;
 }
