5274db8f0b8eaeec5d3aff77c38fb1f335fdea60
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-25 14:49:23 +0900

    [update]playermover

--- Assets/Scripts/Develop/Player/PlayerMover.cs
+++ Assets/Scripts/Develop/Player/PlayerMover.cs
@@ -12,9 +12,9 @@ public class PlayerMover
 
     public void OnMove(Vector2 inputVector, Vector3 cameraScrollVelocity, float deltaTimed)
     {
-        Vector3 inputMove = new Vector3(inputVector.x, inputVector.y * _config.MoveSpeed, 0f);
+        Vector3 inputMove = new Vector3(inputVector.x, inputVector.y, 0f) * _config.MoveSpeed;
         Vector3 scrollMove = new Vector3(0f, cameraScrollVelocity.y, 0f);
-        Vector3 nextPosition = _transform.position + (inputMove + cameraScrollVelocity) * deltaTimed;
+        Vector3 nextPosition = _transform.position + (inputMove + scrollMove) * deltaTimed;
         nextPosition = ClampPositionToCamera(nextPosition);
         nextPosition.z = _transform.position.z;
         _transform.position = nextPosition;
@@ -27,7 +27,7 @@ public class PlayerMover
 
     private Vector3 ClampPositionToCamera(Vector3 position)
     {
-        float depth = position.z - _camera.transform.position.z;
+        float depth = Mathf.Abs(_camera.transform.position.z - position.z);
         // カメラの表示範囲（ワールド座標）
         Vector3 min = _camera.ViewportToWorldPoint(new Vector3(0, 0, depth));
         Vector3 max = _camera.ViewportToWorldPoint(new Vector3(1, 1, depth));
@@ -35,7 +35,7 @@ public class PlayerMover
         Vector3 extents = _collider.bounds.extents;
 
         position.x = Mathf.Clamp(position.x, min.x + extents.x, max.x - extents.x);
-        position.z = Mathf.Clamp(position.z, min.z + extents.z, max.z - extents.z);
+        position.y = Mathf.Clamp(position.y, min.y + extents.y, max.y - extents.y);
 
         return position;
     }
