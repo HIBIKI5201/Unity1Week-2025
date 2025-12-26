257b7cb5d3afa4c883df12ff14058a2370ad55f2
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-26 23:18:08 +0900

    [update]playerconfig

--- Assets/Scripts/Develop/Player/PlayerConfig.cs
+++ Assets/Scripts/Develop/Player/PlayerConfig.cs
@@ -9,9 +9,12 @@ public class PlayerConfig : ScriptableObject
     public float MoveSpeed => _moveSpeed;
     public float GhostTime => _ghostTime;
     public float GhostAbilityCoolTime => _ghostAbilityCoolTime;
+    public int PenetrationCount => _penetrationCount;
+
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private float _attackSpeed = 1f;
     [SerializeField] private float _collisionRadius = 1.5f;
     [SerializeField] private float _ghostTime = 0.5f;
     [SerializeField] private float _ghostAbilityCoolTime = 1f;
+    [SerializeField] private int _penetrationCount = 1;
 }
