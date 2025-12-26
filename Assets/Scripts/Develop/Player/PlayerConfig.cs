2778cfc96f241e84999c1ca74461642570e0cad6
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-26 13:30:44 +0900

    [add]abilitytime

--- Assets/Scripts/Develop/Player/PlayerConfig.cs
+++ Assets/Scripts/Develop/Player/PlayerConfig.cs
@@ -7,7 +7,9 @@ public class PlayerConfig : ScriptableObject
     public float CollisionRadius => _collisionRadius;
     public float AttackSpeed => _attackSpeed;
     public float MoveSpeed => _moveSpeed;
+    public float GhostTime => _ghostTime;
     [SerializeField] private float _moveSpeed = 5f;
     [SerializeField] private float _attackSpeed = 1f;
     [SerializeField] private float _collisionRadius = 1.5f;
+    [SerializeField] private float _ghostTime = 0.5f;
 }
