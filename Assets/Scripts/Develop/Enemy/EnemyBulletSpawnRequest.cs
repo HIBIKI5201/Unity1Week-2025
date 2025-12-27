f0ff657226953673685cdf8ab556551dcb54fabf
kenkatou354 <kenkatou354@gmail.com>
2025-12-28 06:55:31 +0900

    いらない方向を削除

--- Assets/Scripts/Develop/Enemy/EnemyBulletSpawnRequest.cs
+++ Assets/Scripts/Develop/Enemy/EnemyBulletSpawnRequest.cs
@@ -3,14 +3,12 @@ using Unity.Mathematics;
 
 public readonly struct EnemyBulletSpawnRequest : IComponentData
 {
-    public EnemyBulletSpawnRequest(int index,float3 pos,float3 dir)
+    public EnemyBulletSpawnRequest(int index,float3 pos)
     {
         Id = index;
         Position = pos;
-        Direction = dir;
     }
 
     public readonly int Id;
     public readonly float3 Position;
-    public readonly float3 Direction;
 }
