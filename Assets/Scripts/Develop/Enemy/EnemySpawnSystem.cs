9f06feb0187bc3451dfca1370afe5255f44153d0
kenkatou354 <kenkatou354@gmail.com>
2025-12-28 15:21:27 +0900

    [add]エネミースポナーを追加

--- Assets/Scripts/Develop/Enemy/EnemySpawnSystem.cs
+++ Assets/Scripts/Develop/Enemy/EnemySpawnSystem.cs
@@ -3,5 +3,16 @@ using UnityEngine;
 
 public class EnemySpawnSystem : MonoBehaviour
 {
-
+    [SerializeField] private GameObject _enemyPrefab;
+    private float _deltaTime;
+    private void Update()
+    {
+        _deltaTime += Time.deltaTime;
+        if (_deltaTime > 1)
+        {
+            Instantiate(_enemyPrefab);
+            _deltaTime = 0;
+        }
+       
+    }
 }
