cd62ad12d0871b06319f67f381061488d0a2f923
kenkatou354 <kenkatou354@gmail.com>
2025-12-29 22:46:02 +0900

    [add]ホーミング変数を追加

--- Assets/Scripts/Develop/Enemy/EnemyBulletSpawnRequest.cs
+++ Assets/Scripts/Develop/Enemy/EnemyBulletSpawnRequest.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public readonly struct EnemyBulletSpawnRequest : IComponentData
 {
-    public EnemyBulletSpawnRequest(int index,float3 pos,Quaternion quaternion)
+    public EnemyBulletSpawnRequest(int index,float3 pos,int horming)
     {
         Id = index;
         Position = pos;
-        Direction = quaternion;
+        Horming = horming;
     }
 
     public readonly int Id;
     public readonly float3 Position;
-    public readonly Quaternion Direction;
+    public readonly int Horming;
 }
