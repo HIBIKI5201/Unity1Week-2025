c8057ad7cd32aa0947eea0623a859aa180873115
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-12-30 01:57:52 +0900

    [update]ablityname

--- Assets/Scripts/Develop/Ability/AblityName.cs
+++ Assets/Scripts/Develop/Ability/AblityName.cs
@@ -1,10 +1,30 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName ="AblityName",menuName =("ScriptableObjects/AblityName"))]
 public class AblityName : ScriptableObject
 {
-    public AblityTypes[] AblityEntries;
+    [SerializeField] private AblityTypes[] AblityEntries;
+    private Dictionary<AbilityType, string> _cache;
+
+    private void OnEnable()
+    {
+        _cache = new Dictionary<AbilityType, string>();
+        foreach (var e in AblityEntries)
+        {
+            if (!_cache.ContainsKey(e.Ability))
+                _cache.Add(e.Ability, e.Name);
+        }
+    }
+
+    public string GetName(AbilityType type)
+    {
+        if (_cache != null && _cache.TryGetValue(type, out var name))
+            return name;
+
+        return type.ToString();
+    }
 }
 
 [Serializable]
