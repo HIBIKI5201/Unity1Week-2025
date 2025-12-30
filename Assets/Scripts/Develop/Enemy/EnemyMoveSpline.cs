1380270104ee69c9387ac2916bf3fc60b1d6425b
kenkatou354 <kenkatou354@gmail.com>
2025-12-30 20:50:04 +0900

    [add]Typeで使うスプラインを変更する

--- Assets/Scripts/Develop/Enemy/EnemyMoveSpline.cs
+++ Assets/Scripts/Develop/Enemy/EnemyMoveSpline.cs
@@ -1,18 +1,38 @@
 using Unity.Entities.UniversalDelegates;
 using UnityEngine;
 using UnityEngine.Splines;
-
+public enum SplineType
+{
+    None,
+    Wave,
+    Boss,
+    Group
+}
 public class EnemyMoveSpline : MonoBehaviour
 {
-    [SerializeField] private SplineContainer _splineContainer;
+    public SplineType SplineType => _currentType;
+     private SplineCon _splineCon;
     [SerializeField] private float _speed = 5f;
+    private SplineContainer _splineContainer;
+    [SerializeField]
+    private SplineType _currentType = SplineType.None;
+
     private float _splineLength = 0f;
     // 0〜1の範囲で進捗を管理。
     private float _t = 0f;
 
     private void Start()
     {
-        if (_splineContainer == null) return;
+        _splineCon = FindAnyObjectByType<SplineCon>();
+        if (_splineCon == null) return;
+        _splineContainer = _currentType switch
+        {
+            SplineType.Wave => _splineCon.Wave,
+            SplineType.Boss => _splineCon.Boss,
+            SplineType.Group => _splineCon.Group,
+            _ => null
+        };
+        Debug.Log(_currentType);
         _splineLength = _splineContainer.CalculateLength();
     }
 
